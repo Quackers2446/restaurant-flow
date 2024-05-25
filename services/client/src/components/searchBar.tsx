1a1dad21d6746481817c47f727ac5e64058502a3
Matthew Zhou <myzhou@uwaterloo.ca>
2024-05-25 19:05:43 -0400

    add mantine appshell

--- services/client/src/components/searchBar.tsx
+++ services/client/src/components/searchBar.tsx
@@ -1,7 +1,13 @@
-import React from "react"
+import { TextInput } from '@mantine/core';
 
 function SearchBar() {
+    return (
+        <TextInput
+            label="Input label"
+            description="Input description"
+            placeholder="Input placeholder"
+        />
+    );
 }
-
 export default SearchBar
 
