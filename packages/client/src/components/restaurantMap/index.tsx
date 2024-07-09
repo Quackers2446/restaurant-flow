b3e2e5ccc758fe4ef45aeaf9dc355b8a3eb709a1
Luke-zhang-04 <Luke_zhang_04@protonmail.com>
2024-07-09 18:33:37 -0400

    feat(client): change map layer and markers

--- packages/client/src/components/restaurantMap/index.tsx
+++ packages/client/src/components/restaurantMap/index.tsx
@@ -29,13 +29,21 @@ export const RestaurantMap: React.FC = () => {
             scrollWheelZoom
             className={styles.leafletContainer}
         >
-            <TileLayer
+            {/* <TileLayer
                 attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
                 url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
                 maxZoom={20}
+            /> */}
+            <TileLayer
+                attribution='&copy; <a href="https://stadiamaps.com/" target="_blank">Stadia Maps</a> &copy; <a href="https://openmaptiles.org/" target="_blank">OpenMapTiles</a> &copy; <a href="https://www.openstreetmap.org/copyright" target="_blank">OpenStreetMap</a>'
+                url="https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png"
+                maxZoom={20}
             />
             {data?.map((restaurant) => (
-                <Marker position={[restaurant.location.lat, restaurant.location.lng]}>
+                <Marker
+                    key={restaurant.restaurantId}
+                    position={[restaurant.location.lat, restaurant.location.lng]}
+                >
                     <Popup>{restaurant.googleRestaurant.name}</Popup>
                 </Marker>
             ))}
--- packages/client/src/index.tsx
+++ packages/client/src/index.tsx
@@ -3,7 +3,7 @@ import "@mantine/core/styles.css"
 import {useDisclosure} from "@mantine/hooks"
 import {QueryClient, QueryClientProvider} from "@tanstack/react-query"
 import Leaflet from "leaflet"
-import icon from "leaflet/dist/images/marker-icon.png"
+import icon from "./mapMarker.svg"
 import iconShadow from "leaflet/dist/images/marker-shadow.png"
 import "leaflet/dist/leaflet.css"
 import React from "react"
@@ -12,11 +12,14 @@ import {RestaurantMap} from "./components"
 import "./index.scss"
 import reportWebVitals from "./reportWebVitals"
 
+const markerWidth = 25
+const markerHeight = 37
+
 Leaflet.Marker.prototype.options.icon = Leaflet.icon({
     iconUrl: icon,
     shadowUrl: iconShadow,
-    iconAnchor: Leaflet.point(25 / 2, 41),
-    popupAnchor: Leaflet.point(0, -41),
+    iconAnchor: Leaflet.point(markerWidth / 2, markerHeight),
+    popupAnchor: Leaflet.point(0, -markerHeight),
 })
 
 const queryClient = new QueryClient()
