d70b0c933af5661fab3da1bd44c6da6600ce1a6f
Luke-zhang-04 <Luke_zhang_04@protonmail.com>
2024-07-09 16:21:34 -0400

    feat(client): add map markers

--- packages/client/src/components/restaurantMap/index.tsx
+++ packages/client/src/components/restaurantMap/index.tsx
@@ -2,34 +2,43 @@ import styles from "./index.module.scss"
 import {useQuery} from "@tanstack/react-query"
 import qs from "qs"
 import React from "react"
-import {MapContainer, Marker, TileLayer} from "react-leaflet"
+import {MapContainer, Marker, Popup, TileLayer} from "react-leaflet"
 import {type LatLngLiteral} from "leaflet"
 import {apiURL} from "../../globals"
+import {getRestaurantsInAreaResponse} from "../../schema/restaurant"
 
 // TODO: make these modifiable
 const position: LatLngLiteral = {lat: 43.472587, lng: -80.537681}
 
 export const RestaurantMap: React.FC = () => {
-    const {isPending, error, data} = useQuery({
+    const {error, data} = useQuery({
         queryKey: ["restaurantList"],
         queryFn: () =>
             fetch(
                 `${apiURL}/restaurants/in-area?${qs.stringify({lat: position.lat, lng: position.lng, radius: 200})}`,
-            ).then((res) => res.json()),
+            ).then(async (res) => await getRestaurantsInAreaResponse.parseAsync(await res.json())),
     })
+
+    console.log({data, error})
 
     return (
         <MapContainer
             center={position}
-            zoom={50}
+            maxZoom={20}
+            zoom={17}
             scrollWheelZoom
             className={styles.leafletContainer}
         >
             <TileLayer
                 attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
                 url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                maxZoom={20}
             />
-            <Marker position={position}></Marker>
+            {data?.map((restaurant) => (
+                <Marker position={[restaurant.location.lat, restaurant.location.lng]}>
+                    <Popup>{restaurant.googleRestaurant.name}</Popup>
+                </Marker>
+            ))}
         </MapContainer>
     )
 }
--- packages/client/src/index.tsx
+++ packages/client/src/index.tsx
@@ -2,12 +2,22 @@ import {AppShell, Burger, MantineProvider} from "@mantine/core"
 import "@mantine/core/styles.css"
 import {useDisclosure} from "@mantine/hooks"
 import {QueryClient, QueryClientProvider} from "@tanstack/react-query"
+import Leaflet from "leaflet"
+import icon from "leaflet/dist/images/marker-icon.png"
+import iconShadow from "leaflet/dist/images/marker-shadow.png"
 import "leaflet/dist/leaflet.css"
 import React from "react"
 import ReactDOM from "react-dom/client"
 import {RestaurantMap} from "./components"
 import "./index.scss"
 import reportWebVitals from "./reportWebVitals"
+
+Leaflet.Marker.prototype.options.icon = Leaflet.icon({
+    iconUrl: icon,
+    shadowUrl: iconShadow,
+    iconAnchor: Leaflet.point(25 / 2, 41),
+    popupAnchor: Leaflet.point(0, -41),
+})
 
 const queryClient = new QueryClient()
 
